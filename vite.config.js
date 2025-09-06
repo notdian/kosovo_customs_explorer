5c53f1113f88929694da26a9060aff09110b75f2
notdian <dian@fishekqi.com>
2025-09-06 20:45:27 +0200

    switch to preact

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    base: process.env.NODE_ENV === 'production' ? '/kosovo_customs_data/' : '/',
-    resolve: {
-        alias: {
-            "@/data": path.resolve(__dirname, "./data"),
-            "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [preact(), tailwindcss()],
+  base: process.env.NODE_ENV === "production" ? "/kosovo_customs_data/" : "/",
+  resolve: {
+    alias: {
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat", // Must be below test-utils
+      "react/jsx-runtime": "preact/jsx-runtime",
+      "@/data": path.resolve(__dirname, "./data"),
+      "@": path.resolve(__dirname, "./src"),
     },
-    // Inject ISO build time as a constant we can read in the app
-    define: {
-        __BUILD_TIME__: JSON.stringify(new Date().toISOString()),
-    },
-})
+  },
+  // Inject ISO build time as a constant we can read in the app
+  define: {
+    __BUILD_TIME__: JSON.stringify(new Date().toISOString()),
+  },
+});
